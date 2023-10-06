bfc7d5544cd86209b0f7edf3a75bbb5e871c821a
Khai Chan <strubo.gt@gmail.com>
2023-10-06 12:41:32 +0300

    Added checkbox to LocalStorage

--- Web/js/darkmode.js
+++ Web/js/darkmode.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // selectors
 const checkbox = document.getElementById("checkbox");
 
@@ -8,12 +9,36 @@ const theme = localStorage.getItem("theme");
 // on mount
 theme && document.body.classList.add("dark");
 
-
+// Addd an event listener to dark mode
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
-  if (document.body.classList.contains("dark")) { // remembering theme mode
+  
+  //Darkmode to LocalStorage
+  if (document.body.classList.contains("dark")) {
     localStorage.setItem("theme", "dark");
   } else {
   localStorage.removeItem("theme")
   }
 })
+
+
+
+//Checkbox to localsotrage
+
+// Function to save checkbox value to localStorage
+function saveCheckboxState() {
+  const checkbox = document.getElementById('checkbox');
+  localStorage.setItem('checkbox', checkbox.checked);
+}
+
+// Function to load checkbox value from localStorage
+function loadCheckboxState() {
+  const checkbox = document.getElementById('checkbox');
+  checkbox.checked = JSON.parse(localStorage.getItem('checkbox')) || false;
+}
+
+// Add an event listener to the checkbox
+document.getElementById('checkbox').addEventListener('change', saveCheckboxState);
+
+// Load checkbox value from localStorage when the page loads
+loadCheckboxState();
